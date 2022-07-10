028eb2f1a5d52f84babf050cff6581be63fac6d3
badao12x3 <hoanlazy123@gmail.com>
2022-07-10 16:58:15 +0700

    add AcceptMaxBidTest.java and update BaseTest.java

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -6,6 +6,18 @@ import org.json.simple.JSONObject;
 public class BaseTest {
     protected static final JSONObject createdAccount = AuthHelper.createNewExistedRandomUser();
     protected static final String authToken = AuthHelper.getAuthToken(createdAccount);
+    private static JSONObject Account (
+            String email,
+            String password){
+        JSONObject Body = new JSONObject();
 
+        if (email != null)
+            Body.put("email", email);
+        if (password != null)
+            Body.put("password", password);
+        return Body;
+    }
+    protected static final String devUser2_Token = AuthHelper.getAuthToken(Account("devUser2@gmail.com","123456"));
+    protected static final String devUser3_Token = AuthHelper.getAuthToken(Account("devUser3@gmail.com","123456"));
 
 }
